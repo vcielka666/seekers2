8b380798ff7c529d724944f8833ebf4753ce244f
vcielka666 <romanmarko90@gmail.com>
2024-10-04 23:19:36 +0200

    some updates

---
+++ src/app/components/Icons.tsx
@@ -0,0 +1,12 @@
+import { LucideProps } from 'lucide-react'
+
+export const Icons = {
+  logo: (props: LucideProps) => (
+    <img
+    src="/Logo.svg"
+    alt="Logo"
+    width="65" 
+    height="65" 
+  />
+  ),
+}
